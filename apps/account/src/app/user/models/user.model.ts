20a88bc44fb432274843350541fa39dfc1bc53b8
aminov <alami@tshtt.uz>
2022-07-26 11:24:39 +0500

    ch0505 model

---
+++ apps/account/src/app/user/models/user.model.ts
@@ -0,0 +1,18 @@
+import {Document} from "mongoose";
+import {IUser, UserRole} from "@purple/interfaces";
+import {Schema, Prop, SchemaFactory} from "@nestjs/mongoose";
+
+@Schema()
+export class User extends Document implements IUser {
+  @Prop()
+  displayName?: string;
+  @Prop({required: true})
+  email: string;
+  @Prop({required: true})
+  passwordHash: string;
+  @Prop({required: true, enum: UserRole, type: String, default: UserRole.Student})
+  role: UserRole;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
